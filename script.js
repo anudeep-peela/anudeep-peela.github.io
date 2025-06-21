66d8c9d4deefe32a9e0ba4d7a7695e1e2334c569
Anudeep Peela <anudeeppeela9@gmail.com>
2025-06-21 09:08:26 +0530

    Add three.js background

--- script.js
+++ script.js
@@ -23,4 +23,62 @@ document.addEventListener('DOMContentLoaded', function() {
             section.classList.add('visible');
         });
     }
+
+    initBackground();
 });
+
+function initBackground() {
+    const canvas = document.getElementById('bg-canvas');
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.z = 30;
+
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
+    const torus = new THREE.Mesh(geometry, material);
+    scene.add(torus);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(5, 5, 5);
+    scene.add(pointLight);
+    scene.add(new THREE.AmbientLight(0xffffff));
+
+    function addStar() {
+        const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+        const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        const star = new THREE.Mesh(geometry, material);
+        const [x, y, z] = Array(3)
+            .fill()
+            .map(() => THREE.MathUtils.randFloatSpread(200));
+        star.position.set(x, y, z);
+        scene.add(star);
+    }
+    Array(200)
+        .fill()
+        .forEach(addStar);
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    window.addEventListener('resize', onWindowResize);
+
+    function animate() {
+        requestAnimationFrame(animate);
+        torus.rotation.x += 0.01;
+        torus.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+
+    animate();
+}
